dcb527166021a01fc2836e57e31949aeb3b6d8f1
KateDay <khummle@gmail.com>
2019-06-11 21:15:13 -0600

    creating 3 empoyess to each store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: 'Mildred', last_name: 'Honeydew', hourly_rate: 55)
+@store2.employees.create(first_name: 'Peppa', last_name: 'York', hourly_rate: 48)
+@store1.employees.create(first_name: 'Dewey', last_name: 'Grant', hourly_rate: 60)
+@store2.employees.create(first_name: 'Philip', last_name: 'Potter', hourly_rate: 40)
+@store1.employees.create(first_name: 'Clemintine', last_name: 'Homes', hourly_rate: 50)
+@store2.employees.create(first_name: 'George', last_name: 'Lochart', hourly_rate: 50)
