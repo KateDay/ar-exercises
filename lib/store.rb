e7d08d82c6fe2c8e67e934d2e04f8bc0a73fa0dc
KateDay <khummle@gmail.com>
2019-06-12 08:51:42 -0600

    set up validators and error messages

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
-    has_many :employees
-end
+  has_many :employees
+  validates :name, length: {minimum: 3, :message => "The name of your store must have at least 3 characters"}
+  validates_numericality_of :annual_revenue, {:only_integer => true, :message => "The annual_revenue needs to be a number"}
+	
+	def validation_message
+		if Store.errors.any?
+			Store.errors.each do |attribute, message|
+				puts "Error: Sorry but #{attribute}: #{message}"
+			end
+		end
+	end
+
+end	
+
